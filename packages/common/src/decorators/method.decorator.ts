c225362d26c5f4d589123de4622c19a1d45064b4
Capgop <capgop0@gmail.com>
2020-06-01 17:14:43 +0500

    fix(common): set metadata to class method

--- packages/common/src/decorators/method.decorator.ts
+++ packages/common/src/decorators/method.decorator.ts
@@ -2,6 +2,6 @@ import { METADATA_KEY } from '../constants';
 
 export function Method(name: string): MethodDecorator {
     return (target: any, propertyKey: string | symbol) => {
-        Reflect.defineMetadata(METADATA_KEY.method, { name, target, key: propertyKey }, target);
+        Reflect.defineMetadata(METADATA_KEY.method, { name, target, key: propertyKey }, target[propertyKey]);
     }
 }
