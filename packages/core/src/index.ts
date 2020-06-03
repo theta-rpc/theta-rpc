f8c8914f1cee37dbfb9b3cc1c9a8e40f9883f434
Capgop <capgop0@gmail.com>
2020-06-03 14:04:24 +0500

    core package. Base implementation

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,4 @@
+export * from './interfaces';
+
+export * from './server';
 export * from './factory';
--- packages/core/src/interfaces.ts
+++ packages/core/src/interfaces.ts
@@ -2,3 +2,23 @@ export interface IJsonRPCFactoryOptions {
     server: any,
     procedures: any[]
 }
+
+export interface IJsonRPCServerOptions {
+    hostname?: string,
+    port: number
+}
+
+export interface ITransport {
+    readonly name: string;
+
+    listen(port: number): void;
+
+    close(): void;
+
+    onListening(listener: () => any): void;
+    onError(listener: (err: Error) => any): void;
+    onClose(listener: () => void): void;
+
+    up(): void;
+    down(): void;
+}
