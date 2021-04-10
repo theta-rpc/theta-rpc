cab2ebafcdef7d18bcaa291515b5a6e67d3310e9
mhw0 <mhw0@yahoo.com>
2021-04-10 21:49:11 +0500

    test(http-transport): fix

--- packages/http-transport/test/http-transport.test.ts
+++ packages/http-transport/test/http-transport.test.ts
@@ -13,10 +13,11 @@ function makeRequest() {
 describe('HTTP transport', () => {
   const transport = new HTTPTransport({ port: 1024, cors: { origin: '*' } });
   const fakeCb = sinon.stub();
-  transport.onRequest(fakeCb);
+  transport.on("message", fakeCb);
 
-  before( async () => {
-    await transport.start();
+  before((done) => {
+    transport.emit('start');
+    transport.on('started', done);
   });
 
   it('should be a subclass of ThetaTransport', () => {
@@ -25,7 +26,7 @@ describe('HTTP transport', () => {
 
   it('should respond to the request with correct headers', async () => {
     fakeCb.callsFake(async(data: Buffer, transportContext: HTTPTransportContext) => {
-      await transport.reply(data.toString(), transportContext);
+      await transport.emit("reply", data.toString(), transportContext);
     })
 
     const response = await makeRequest();
@@ -40,7 +41,7 @@ describe('HTTP transport', () => {
 
   it('should close the connection with status code 204 when response data is null', async () => {
     fakeCb.callsFake(async(data: Buffer, transportContext: HTTPTransportContext) => {
-      await transport.reply(null, transportContext);
+      await transport.emit("reply", null, transportContext);
     })
 
     const response = await makeRequest();
@@ -51,21 +52,22 @@ describe('HTTP transport', () => {
   });
 
 
-  it('custom express instance', async () => {
+  it('attach to existing express app', async () => {
     const expressApp = express();
-    const transport2 = new HTTPTransport({ express: expressApp, path: '/test-route' });
-    transport2.onRequest((data, context) => {
-      transport2.reply('ok', context);
+    const transport2 = HTTPTransport.attach(expressApp);
+    transport2.on("message", (data, context) => {
+      transport2.emit("reply", 'ok', context);
     });
     const server = expressApp.listen(1025);
-    const response = await axios.post('http://localhost:1025/test-route');
+    const response = await axios.post('http://localhost:1025');
     assert.equal(response.status, 200);
     assert.equal(response.data, 'ok');
     server.close();
   });
 
-  after( async () => {
-    await transport.stop();
+  after((done) => {
+    transport.emit('stop');
+    transport.on('stopped', done);
   });
 
 });
