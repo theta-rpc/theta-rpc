5b977a3f32c01cc7ac8f0c3f6ec3b689eed93aa6
mhw0 <mhw0@yahoo.com>
2021-04-10 21:24:00 +0500

    websocket-transport: use events

--- packages/websocket-transport/src/websocket.transport.ts
+++ packages/websocket-transport/src/websocket.transport.ts
@@ -13,62 +13,65 @@ export class WebSocketTransport extends ThetaTransport {
   private wss: WebSocket.Server;
 
   constructor(private options: IWebSocketTransportOptions) {
-    super('WebSocket transport');
+    super("WebSocket transport");
 
     const httpServer = http.createServer();
-    const wss = new WebSocket.Server({ server: httpServer, path: options.path || '/' });
+    const wss = new WebSocket.Server({
+      server: httpServer,
+      path: options.path || "/",
+    });
 
     this.httpServer = httpServer;
     this.wss = wss;
     this.handleErrors();
+    this.listen();
+    this.listenConnection();
   }
 
-  public reply(data: any, transportContext: IWebSocketTransportContext): Promise<void> {
+  private listen() {
+    this.on("start", () => this.start());
+    this.on("stop", () => this.stop());
+    this.on("reply", (data, context) => this.reply(data, context));
+  }
+
+  public reply(data: any, transportContext: IWebSocketTransportContext) {
     const connection = transportContext.getConnection();
-    return new Promise((resolve, reject) => {
-      if(data) {
-        connection.send(JSON.stringify(data), (error?: Error) => {
-          if(error) {
-            debug(error);
-            reject();
-            return;
-          }
-          resolve();
-        });
-      }
+    if (data) {
+      connection.send(JSON.stringify(data), (err?: Error) => {
+        if(error) debug(error);
+      });
+    }
+  }
+
+  public listenConnection() {
+    this.wss.on("connection", (socket) => {
+      socket.on("message", (data) => {
+        const context = this.createContext(socket);
+        this.emit("message", data, context);
+      });
     });
   }
 
-  public onRequest(cb: (data: any, transportContext: any) => any) {
-    this.wss.on('connection', (socket) => {
-      socket.on('message', (data) => {
-        const context = this.createContext(socket);
-        cb(data, context);
-      })
-    })
-  }
-
   private handleErrors() {
-    this.wss.on('error', (error) => {
+    this.wss.on("error", (err?: Error) => {
       debug(error);
-    })
+    });
   }
 
   private createContext(connection: WebSocket) {
     return new WebSocketTransportContext(connection);
   }
 
-  public start(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.httpServer.listen(this.options.port, this.options.hostname, resolve).once('error', reject);
+  public start() {
+    const { port, hostname } = this.options;
+    this.httpServer.listen(port, hostname, (err?: Error) => {
+      if(!err) this.emit("started")
     });
   }
 
-  public stop(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.httpServer.close((err) => {
-        err ? reject() : resolve();
-      });
+  public stop() {
+    this.httpServer.close((err) => {
+      if(!err) this.emit("stopped");
     });
   }
 }
