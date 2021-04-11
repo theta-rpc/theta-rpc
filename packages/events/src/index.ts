218b5aa4478c59d63ef3e8a550335ef2b4805934
mhw0 <mhw0@yahoo.com>
2021-04-11 16:28:27 +0500

    feat(events): add `toPromise` method

--- packages/events/src/index.ts
+++ packages/events/src/index.ts
@@ -1,1 +1,29 @@
-export { EventEmitter } from 'eventemitter3';
+import events from "eventemitter3";
+
+export class EventEmitter<
+  EventTypes extends events.ValidEventTypes
+> extends events.EventEmitter<EventTypes> {
+  constructor() {
+    super();
+  }
+
+  public toPromise<T extends events.EventNames<EventTypes>>(
+    event: T,
+    timeout: number = 2000
+  ): Promise<any> {
+    let emitted: boolean = false;
+
+    return new Promise((resolve, reject) => {
+      //FIXME
+      //@ts-ignore
+      this.on(event, (...args: any[]) => {
+        resolve(args);
+        emitted = true;
+      });
+
+      setTimeout(() => {
+        if (!emitted) reject(new Error("Timeout error"));
+      }, timeout);
+    });
+  }
+}
