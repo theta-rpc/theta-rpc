889457e8783a0a5de38ecce5e5f5a6240d8b780b
mhw0 <mhw0@yahoo.com>
2021-04-25 23:49:29 +0500

    fix(mqtt-transport): check if client exists

--- packages/mqtt-transport/src/mqtt.transport.ts
+++ packages/mqtt-transport/src/mqtt.transport.ts
@@ -8,12 +8,13 @@ const debug = createDebug("THETA-RPC:MQTT-TRANSPORT");
 
 export class MQTTTransport extends ThetaTransport {
   private client!: mqtt.Client;
+
   constructor(private options: IMQTTTransportOptions) {
     super("MQTT transport");
-    this.listen();
+    this.listenEvents();
   }
 
-  private listen() {
+  private listenEvents() {
     this.on("reply", (data) => this.reply(data));
     this.on("start", () => this.connect());
     this.on("stop", () => this.disconnect());
@@ -40,13 +41,16 @@ export class MQTTTransport extends ThetaTransport {
           this.emit("message", payload, context);
         });
       })
-      .on("error", (err) => this.handleError(err));
+      .on("error", this.handleError);
   }
 
   private createContext(packet: mqtt.Packet) {
     return new MQTTTransportContext(packet);
   }
+
   private disconnect() {
-    this.client.end(false, undefined, () => this.emit("stopped"));
+    if (this.client) {
+      this.client.end(false, undefined, () => this.emit("stopped"));
+    }
   }
 }
