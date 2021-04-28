1a4cc8efc0df122f0d07762fd20d4ec3584cd518
mhw0 <mhw0@yahoo.com>
2021-04-28 09:28:41 +0500

    fix(core): fix request context type

--- packages/core/src/method/types.ts
+++ packages/core/src/method/types.ts
@@ -1,7 +1,7 @@
 import { RequestContextType } from '../server';
 
 export type AccessorType = (context: RequestContextType) => Promise<boolean> | boolean;
-export type HandlerType = (context: RequestContextType) => any;
+export type HandlerType = (context: RequestContextType<any>) => any;
 
 export type MethodObjectType = {
   method: string,
