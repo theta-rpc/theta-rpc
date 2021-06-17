ac205ef92215ec76ad1932e1826ac6ea75cdc073
mhw0 <mhw0@yahoo.com>
2021-06-17 16:10:50 +0500

    json-rpc: rename "BadTransportDataException" to "InvalidMessageTypeException"

--- packages/json-rpc/src/exceptions/index.ts
+++ packages/json-rpc/src/exceptions/index.ts
@@ -1,10 +1,10 @@
 export * from './types';
 
-export * from './json-rpc.exception';
 export * from './internal-error.exception';
+export * from './invalid-message-type.exception';
 export * from './invalid-params.exception';
 export * from './invalid-request.exception';
+export * from './json-rpc.exception';
 export * from './method-not-found.exception';
-export * from './parse-error.exception';
 export * from './no-access-to-method.exception';
-export * from './bad-transport-data.exception';
+export * from './parse-error.exception';
